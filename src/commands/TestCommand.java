9a4498ae2da11f732bf8e226e081759d8d2a8d5b
Michael Costello <mwcostel@ncsu.edu>
2023-03-21 12:32:54 -0400

    Implemented script command to run a bash script.

---
+++ src/commands/TestCommand.java
@@ -0,0 +1,17 @@
+package commands;
+
+@Command(name = "TestCommand", description = "Test function to help understand picoCLI", mixinStandardHelpOptions = true)
+public class TestCommand implements Runnable {
+
+    @Parameters(paramLabel = "<word>", defaultValue = "default", description = "Message to append to print statement")
+    private String[] message = { "default" };
+
+    public void run() {
+        System.out.println("Command Test ran " + message);
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new TestCommand()).execute(args);
+        System.exit(exitCode);
+    }
+}
