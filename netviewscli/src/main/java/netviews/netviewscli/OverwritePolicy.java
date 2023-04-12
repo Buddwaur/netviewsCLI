247d3d3a9a92e7d14cc3df4df58a95f60eda296c
iahandy <iahandy@ncsu.edu>
2023-04-12 18:59:35 -0400

    Changed overwritepolicy to use a Socket

--- netviewscli/src/main/java/netviews/netviewscli/OverwritePolicy.java
+++ netviewscli/src/main/java/netviews/netviewscli/OverwritePolicy.java
@@ -6,40 +6,36 @@ import picocli.CommandLine.Command;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 /*
  * The SOAP client code in this command was used from a stackoverflow
  * forum response at:
  * https://stackoverflow.com/questions/15940234/how-to-do-a-soap-web-service-call-from-java-class
  */
-@Command ( name = "OverwritePolicy",
-		description = "Takes filepath of a new policy json file to overwrite current policy graph.",
-        mixinStandardHelpOptions = true )
+@Command(name = "OverwritePolicy", description = "Takes filepath of a new policy json file to overwrite current policy graph.", mixinStandardHelpOptions = true)
 public class OverwritePolicy implements Runnable {
-	// TODO: get rid of default value? Might not make sense in this context
-    // @Parameters ( paramLabel = "<policy filepath>", defaultValue = "default",
-    //        description = "file path to new policy json file." )
-    //private final String[] filepath = {"default"};
+
+    private Socket sock;
 
     @Override
-    public void run () {
-    	final Runtime runTimeObj = Runtime.getRuntime();
-    	try {
-    		Process process = runTimeObj.exec("nc 127.0.0.1 9191 && creatPolicy");
-    		BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            // Read the output from the command
-            System.out.println("Here is the standard output of the command:\n");
-            String s = null;
-            while ((s = stdInput.readLine()) != null) {
-                System.out.println(s);
-            }
-    	} catch (final IOException e) {
+    public void run() {
+        // Connect to Server with Socket
+        try {
+            sock = new Socket("127.0.0.1", 9191);
+            PrintStream output = new PrintStream(sock.getOutputStream());
+            output.println("Change!");
+            output.close();
+            sock.close();
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
-    	}
-    	
-    	
-    	//System.out.println("Overwrite command run, filepath = " + String.join(" ", filepath));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
 }
